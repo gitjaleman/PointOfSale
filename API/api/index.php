f4df9c6c93d609ec73967ec40bd025770aa3668c
l <javiersettex@gmail.com>
2022-07-09 20:15:27 -0500

    files

--- API/api/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-chdir( dirname(__DIR__) );
-define("SYS_PATH","lib/");
-define("APP_PATH","app/");
-require SYS_PATH."initial.php";
-$app =new Api;
-?>
---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="app/assets/css/semantic.css">
+  <link rel="stylesheet" href="app/assets/css/Semantic-UI-Alert.css">
+  <link rel="stylesheet" href="login.css">
+  <title>Document</title>
+</head>
+<body>
+  
+</body>
+<script src="app/assets/js/jquery.min.js"></script>
+<script src="app/assets/js/semantic.js"></script>
+<script src="app/assets/js/Semantic-UI-Alert.js"></script>
+</html>
