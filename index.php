47ce0bf1b1c17826c3ec8b02d10fbd77d41a8a6f
l <javiersettex@gmail.com>
2022-07-09 22:58:24 -0500

    init login

--- index.php
+++ index.php
@@ -1,18 +1,37 @@
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="app/assets/css/semantic.css">
   <link rel="stylesheet" href="app/assets/css/Semantic-UI-Alert.css">
-  <link rel="stylesheet" href="login.css">
+  <link rel="stylesheet" href="app/assets/css/config.css">
+  <link rel="stylesheet" href="app/assets/css/login.css">
   <title>Document</title>
 </head>
+
 <body>
-  
+
+
+  <form class="ui form ">
+    <img src="app/assets/images/login/login.png">
+    <div class="ui left icon input W100 Mb_15">
+      <input type="text" placeholder="Usuario">
+      <i class="user icon"></i>
+    </div>
+    <div class="ui left icon input W100 Mb_15">
+      <input type="text" placeholder="Contraseña">
+      <i class="unlock icon"></i>
+    </div>
+    <button class="ui violet button W100">IINGRESO</button>
+    <div class="ui loading button W100">Conectando</div>
+  </form>
+
 </body>
 <script src="app/assets/js/jquery.min.js"></script>
 <script src="app/assets/js/semantic.js"></script>
 <script src="app/assets/js/Semantic-UI-Alert.js"></script>
+
 </html>
